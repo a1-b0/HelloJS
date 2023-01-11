b3fee2153789a916609505844a894e18965c490e
a1-b0 <iamalexbarker@gmail.com>
2023-01-11 18:52:02 +0300

    1 commit in branch

--- Hello.js
+++ Hello.js
@@ -1,5 +1,12 @@
-
+//my comments
 let a = 10;
 let b = 20;
 let result = a + b;
 console.log(result);
+
+function culc (a, b, sign) {
+    let res = a + b
+    if(sign == "+")
+        res = a + b
+    return res
+}
